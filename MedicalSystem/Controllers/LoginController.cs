4f0f657620ea3c4159dc76c682278a07f2665575
Serena Aruci <serenaa.aruci@gmail.com>
2024-05-01 16:06:44 +0200

    Add project files.

---
+++ MedicalSystem/Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MedicalSystem.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
